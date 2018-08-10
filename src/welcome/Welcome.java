1ce26bd9f99473812f78d035833a6eef1728022f
Kishore <Owner@HARI-P3IVR4V>
2018-08-10 16:20:58 +0530

    This is first java program uploading on git. (Testing purpose)

---
+++ src/welcome/Welcome.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package welcome;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Owner
+ */
+public class Welcome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Welcome from HARI!","Greetings",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Your activity is being monitored.","Warning!",JOptionPane.WARNING_MESSAGE);
+    }
+    
+}
